d1cc639bc43d272de5dbc8dd382c155e4246d3a9
Wang Yu <sanjay_wang@outlook.com>
2021-03-01 12:03:01 +0900

    fixed a bug about paste_feature

--- exkaldi/core/feature.py
+++ exkaldi/core/feature.py
@@ -29,7 +29,7 @@ from exkaldi.utils.utils import FileHandleManager
 from exkaldi.utils import declare
 from exkaldi.core.archive import BytesFeat,BytesCMVN,ListTable,IndexTable
 from exkaldi.core.load import load_list_table,load_index_table
-from exkaldi.core.common import check_multiple_resources,run_kaldi_commands_parallel,run_kaldi_commands
+from exkaldi.core.common import check_multiple_resources,run_kaldi_commands_parallel,run_shell_command
 
 def __compute_feature(target,kaldiTool,useSuffix=None,name="feat",outFile=None):
 	'''
